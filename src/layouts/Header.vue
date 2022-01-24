efeca5eb85db5fae6a806f796a22933301959627
jaeho choi <chlwogh1993@gmail.com>
2022-01-24 13:46:46 +0900

    commit

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,67 +1,45 @@
 <template>
-  <v-app>
-    <v-card
-      color="orange"
-      flat
+  <div>
+    <v-app-bar
+      color="blue lighten-2"
+      dense
+      dark
     >
-      <v-app-bar
-        color="blue lighten-1"
-        dark
-        dense
+      <v-menu
+        right
+        bottom
       >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
 
-        <v-app-bar-nav-icon @click="drawer = !drawer" />
-        <v-toolbar-title>Vue Study</v-toolbar-title>
-        <v-spacer />
-      </v-app-bar>
-    </v-card>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-      color="blue lighten-5"
-    >
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://tistory3.daumcdn.net/tistory/3932093/attach/011129f3c339412f95e66b56125cb452"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Vue Study</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider />
-
-      <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          :to="item.to"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <router-view />
-  </v-app>
+        <v-list>
+          <v-list-item
+            v-for="(item,idx) in items"
+            :key="idx"
+            :to="item.to"
+          >
+            <v-list-item-icon>
+                <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title class="mr-15"> {{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-toolbar-title>Vue sample</v-toolbar-title>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 import { get } from 'vuex-pathify'
 export default {
-  data() {
-    return {
-      drawer: true,
-    }
-  },
   computed: {
     items: get('app/items'),
   },
