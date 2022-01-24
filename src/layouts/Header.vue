3accc125ddae1e9d57fbfffa8ccee6c9a8f7f646
jaeho choi <chlwogh1993@gmail.com>
2022-01-24 13:44:02 +0900

    commit

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -21,14 +21,14 @@
 
         <v-list>
           <v-list-item
-            v-for="(n,idx) in items"
+            v-for="(item,idx) in items"
             :key="idx"
-            :to="n.to"
+            :to="item.to"
           >
             <v-list-item-icon>
-                <v-icon v-if="n.icon">{{ n.icon }}</v-icon>
+                <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title class="mr-15"> {{ n.title }}</v-list-item-title>
+            <v-list-item-title class="mr-15"> {{ item.title }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
